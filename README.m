f2ac9db370fcc49816bcf2127ca32742de723907
yusufsalehin <145336527+yusufsalehin@users.noreply.github.com>
2024-02-10 03:18:10 +0600

    Update README.m

--- README.m
+++ README.m
@@ -1,5 +1,5 @@
  I’m @yusufsalehin
  I’m interested in working on machine learning , deep learning .
- I’m currently  studying BSC in CSE (Major - Data Science).My gradution will be completed by June 2023.
+ I’m currently  studying BSC in CSE (Major - Data Science).My gradution will be completed by 2024.
  I am an undergraduate teaching assistant (UTA) at East West University ,Bangladesh.
  I am looking  forward to working with artificial intelligence,machine learing ,deep learning projects.I would love to work with python.
