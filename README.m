2ad17a10882971f7a2403d2c38db1ff2f4f6eac9
Yusuf Salehin <yusufsalehin211186@gmail.com>
2026-07-30 06:06:55 +0600

    Update README.m

--- README.m
+++ README.m
@@ -1,2 +1,5 @@
- I’m @yusufsalehin
-I hold  a B.Sc. and an M.Sc. in Computer Science and Engineering with a major in Data Science from East West University. Passionate about Artificial Intelligence, with interests in AI Agents, Large Language Models (LLMs), Machine Learning, Deep Learning, and Natural Language Processing. Skilled in Python and modern AI frameworks, with hands-on experience building intelligent AI applications. Eager to contribute as an AI Engineer by developing innovative, real-world AI solutions.
+I’m @yusufsalehin
+M.Sc. and B.Sc. in CSE (Data Science) East West University. 
+Passionate about Artificial Intelligence, with interests in AI Agents, Large Language Models (LLMs), Machine Learning, Deep Learning, and Natural Language Processing.
+Skilled in Python and modern AI frameworks, with hands-on experience building intelligent AI applications.
+Eager to contribute as an AI Engineer by developing innovative, real-world AI solutions.
