8b50b06b780f0017532b9608f81b3b1cbe257d31
Yusuf Salehin <yusufsalehin211186@gmail.com>
2026-07-30 06:04:22 +0600

    Update README.m

--- README.m
+++ README.m
@@ -1,4 +1,2 @@
  I’m @yusufsalehin
- I’m interested in working on Machine Learning , Deep Learning,AI Ajent system  ,NLP .
- I’ve completed my  BSC and MSC at East West University in CSE (Major - Data Science) .
- I am looking  forward to working with artificial intelligence in industry sector along with full stack development.
+I hold  a B.Sc. and an M.Sc. in Computer Science and Engineering with a major in Data Science from East West University. Passionate about Artificial Intelligence, with interests in AI Agents, Large Language Models (LLMs), Machine Learning, Deep Learning, and Natural Language Processing. Skilled in Python and modern AI frameworks, with hands-on experience building intelligent AI applications. Eager to contribute as an AI Engineer by developing innovative, real-world AI solutions.
